c2b9349bc823e5e884e471432a6fd1654f90a5c0
pedroalles <pedroalles@gmail.com>
2021-11-17 19:42:41 -0300

    refactor: extract axios request to function

--- server/test/integration/posts.spec.js
+++ server/test/integration/posts.spec.js
@@ -6,17 +6,17 @@ const generate = () => {
     return crypto.randomBytes(20).toString('hex');
 };
 
+const request = (url, method, data) => {
+    return axios({ url, method, data });
+};
+
 test('should get posts', async () => {
 
     const post1 = await postsService.savePost({ title: generate(), content: generate() });
     const post2 = await postsService.savePost({ title: generate(), content: generate() });
     const post3 = await postsService.savePost({ title: generate(), content: generate() });
 
-    const response = await axios({
-        url: 'http://localhost:3000/posts',
-        method: 'get'
-    });
-
+    const response = await request('http://localhost:3000/posts', 'get');
     const posts = response.data;
     expect(posts).toHaveLength(3);
 
