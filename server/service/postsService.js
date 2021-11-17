4a38318976ba262f5b22cc65d60a918c2937f0d3
pedroalles <pedroalles@gmail.com>
2021-11-17 16:49:40 -0300

    feat: add deletePost service

--- server/service/postsService.js
+++ server/service/postsService.js
@@ -8,7 +8,12 @@ const savePost = async (post) => {
     return postsData.savePost(post);
 };
 
+const deletePost = async (id) => {
+    return postsData.deletePost(id);
+};
+
 module.exports = {
     getPosts,
-    savePost
+    savePost,
+    deletePost
 };
