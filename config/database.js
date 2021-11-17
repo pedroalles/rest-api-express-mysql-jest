94e10a1041b666480974857a12354197f98ae6d7
pedroalles <pedroalles@gmail.com>
2021-11-17 16:23:08 -0300

    chore: create database connection configs

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+};
