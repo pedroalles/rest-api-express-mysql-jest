5940c457c8b6c0a948b7b42dd1e08c3a9b3314a2
pedroalles <pedroalles@gmail.com>
2021-11-17 19:20:23 -0300

    refactor: optimize get posts test

--- server/test/integration/posts.spec.js
+++ server/test/integration/posts.spec.js
@@ -1,18 +1,26 @@
 const crypto = require('crypto');
 const axios = require('axios');
+const postsService = require('../../service/postsService');
 
 const generate = () => {
     return crypto.randomBytes(20).toString('hex');
 };
 
 test('should get posts', async () => {
+
+    const post1 = await postsService.savePost({ title: generate(), content: generate() });
+    const post2 = await postsService.savePost({ title: generate(), content: generate() });
+    const post3 = await postsService.savePost({ title: generate(), content: generate() });
+
     const response = await axios({
         url: 'http://localhost:3000/posts',
         method: 'get'
     });
+
     const posts = response.data;
-    const [firstPost] = posts;
     expect(posts).toHaveLength(3);
-    expect(firstPost.id).toBe(1);
-    expect(firstPost.title).toBe('REST API: Métodos');
+
+    await postsService.deletePost(post1.id);
+    await postsService.deletePost(post2.id);
+    await postsService.deletePost(post3.id);
 });
